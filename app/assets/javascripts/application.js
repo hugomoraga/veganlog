19f461051a442e73171358dbb52b629a4756dde3
kndl <hg.moraga@gmail.com>
2020-03-07 19:58:31 -0300

    fixed jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery/dist/jquery
+//= require jquery
+//= require jquery_ujs
 //= require flickity/dist/flickity.pkgd.js
 //= require_tree .
